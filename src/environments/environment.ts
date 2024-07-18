0bb3e7b46503e0315ae04a2739d4b0c27d4fbfe9
Swapnil Opash <sra.opash@gmail.com>
2024-07-18 15:59:38 +0530

    changed conference link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
   domain: '.wheels.tube',
   wasabiUrl: wasabiUrl,
   socketUrl: `${backendUrl}`,
-  conferenceUrl: 'https://facetime.tube/',
+  conferenceUrl: 'https://meet.facetime.tube/',
   logoutUrl: logoutUrl
 
 };
